e052aa06ab594067cf3f9a83c1c0e42be3c3a3a9
“karthik.ravikumar1” <karthi@Karthiks-MacBook-Air.local>
2024-07-02 23:28:05 +0530

    Apollo Server setup and run

---
+++ index.js
@@ -0,0 +1,33 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// types
+import { typeDefs } from "./schema.js";
+
+import db from "./_db.js";
+
+const resolvers = {
+    Query: {
+        games(){
+            return db.games
+        },
+        authors(){
+            return db.authors
+        },
+        reviews(){
+            return db.reviews
+        },
+    }
+}
+
+// server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: {port: 4000}
+})
+ 
+console.log('Server ready at port', 4000);
