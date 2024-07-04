6a6382c343499a32512daa00de80c4f38bfb26fd
“karthik.ravikumar1” <karthi@Karthiks-MacBook-Air.local>
2024-07-04 13:37:30 +0530

    GrphQL Query multiple resources & fetch by Foreign Key

--- index.js
+++ index.js
@@ -7,27 +7,52 @@ import { typeDefs } from "./schema.js";
 import db from "./_db.js";
 
 const resolvers = {
-    Query: {
-        games(){
-            return db.games
-        },
-        authors(){
-            return db.authors
-        },
-        reviews(){
-            return db.reviews
-        },
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game:{
+    review(parent){
+      return db.reviews.filter(r=> r.game_id === parent)
     }
-}
+  },
+  Author:{
+    review(parent){
+      return db.reviews.filter(r=> r.author_id === parent)
+    }
+  },
+  Review:{
+    author(parent){
+      return db.reviews.filter(r=> r.author_id === parent)
+    },
+    game(parent){
+      return db.reviews.filter(r=> r.game_id === parent)
+    }
+  },
+};
 
 // server setup
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
-
 const { url } = await startStandaloneServer(server, {
-    listen: {port: 4000}
-})
- 
-console.log('Server ready at port', 4000);
+  listen: { port: 4000 },
+});
+console.log("Server ready at port", 4000);
--- schema.js
+++ schema.js
@@ -3,21 +3,28 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        review: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content:String!
+        author: [Author!]
+        game:[Game]
     }
     type Author {
         id: ID!
         name: String!
         verified:Boolean!
+        review: [Review]
     }
     type Query {
         reviews:[Review]
+        review(id:ID!): Review
         games:[Game]
+        game(id:ID!):Game
         authors:[Author]
+        author(id:ID!):Author
     }
 `
     
